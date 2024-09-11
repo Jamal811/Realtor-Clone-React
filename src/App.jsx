4c1ad21c3686400262660316d9cb6f1e7cb0acde
Jamalshah <jimisk811@gmail.com>
2024-09-11 11:59:30 +0530

    Header section created

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
