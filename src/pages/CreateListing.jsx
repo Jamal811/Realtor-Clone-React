ea9235b967516d39853123758239c786e42df279
Jamalshah <jimisk811@gmail.com>
2024-09-13 18:01:51 +0500

    take a break from creating ui of listing

---
+++ src/pages/CreateListing.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const CreateListing = () => {
+  const [formData, setFormData] = useState({
+    type: "rent",
+  });
+  const { type } = formData;
+  const onChange = () => {};
+  return (
+    <main className="max-w-md px-2 mx-auto">
+      <h1 className="text-3xl text-center mt-6 font-bold">Create a Listing</h1>
+      <form>
+        <p className="text-lg mt-6 font-semibold">Sell / Rent</p>
+        <div className="flex">
+          <button
+            type="button"
+            id="type"
+            value="sale"
+            onClick={onChange}
+            className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+              type === "rent"
+                ? "bg-white text-black"
+                : "bg-slate-600 text-white"
+            }`}
+          >
+            sell
+          </button>
+          <button
+            type="button"
+            id="type"
+            value="rent"
+            onClick={onChange}
+            className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
+              type === "sale"
+                ? "bg-white text-black"
+                : "bg-slate-600 text-white"
+            }`}
+          >
+            rent
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default CreateListing;
