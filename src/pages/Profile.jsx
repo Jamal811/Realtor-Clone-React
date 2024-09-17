b2e0386eec3cd430e2d339c3636822d29243dc0d
Jamalshah <jimisk811@gmail.com>
2024-09-17 22:09:01 +0500

    complete listingitem component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -145,7 +145,7 @@ function Profile() {
             <h2 className="text-2xl text-center font-semibold mb-6">
               My Listings
             </h2>
-            <ul className="">
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
